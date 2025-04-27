ba657c659905ad4c1a651b2d0aff4931097e83f3
ltb <ltb@DESKTOP-FO8H3TR>
2025-04-28 02:13:45 +0800

    添加后处理

--- matlab/loadresult.m
+++ matlab/loadresult.m
@@ -1,7 +1,15 @@
 
 clear;
-result = readmatrix("..\text.txt");
 
-pcolor(abs(reshape(result(1:101*101,15),101,101)));
+filename = "out.h5" ; 
+
+x = h5read(filename , '/x');
+y = h5read(filename , '/y');
+nx = length(x); 
+ny = length(y);
+
+fieldAbs = h5read(filename , '/fieldAbs');
+
+pcolor(reshape(fieldAbs(1:nx*ny,5),nx,[]))
 shading interp
 colormap jet
